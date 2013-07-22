0a62ed851d6823f72eaf9009ab649544a8054aa8
Hao <echowuhao@gmail.com>
2013-07-21 21:49:57 -0500

    mitbbs fetch

--- lib/Mitbbs.pm
+++ lib/Mitbbs.pm
@@ -1,29 +1,35 @@
 package Mitbbs;
 use v5.14;
 use Dancer2 ':syntax';
-#use Mitbbs::Article;
+use Mitbbs::Article;
 use Mojo::UserAgent;
 use Encode;
-package Article {
-use Moo;
-	has 'link' => ( is => 'ro');
-	has 'title'=> ( is => 'ro');
-}
+
 
 my $ua = Mojo::UserAgent->new;
 
-my $body = $ua->get('http://www.mitbbs.com/bbsdoc/RuralChina.html')->res->dom;
-
-my $collection = $body->find('a[href^="/article"]');
-
-my @articles = $collection->map(sub { Article->new(link => $_->{href}, title => decode('gb2312',$_->text)) } )->each;
+sub pull_articles {
+    my $mitbbs_root = 'http://www.mitbbs.com/bbsdoc/';
+    my $full_href = $mitbbs_root . shift . '.html';
+    debug "the full href is $full_href"; 
+    
+    my $collection = $ua->get($full_href)->res->dom->find('a[href^="/article_t/"]');
+    #TODO: get sub out
+    my @articles = $collection->map(sub { Mitbbs::Article->new(link => $_->{href}, title => decode('gb2312',$_->text)) } )->each;
+    
+    return @articles[0..20];
+}
 
 prefix '/mitbbs' => sub {
-
-    get '/' => sub {
-        
-	return template 'mitbbs/index.tt', { articles => \@articles };
+   
+    get '/:bankuai' => sub {
+    my $bankuai = params->{bankuai};
+    my @articles = pull_articles($bankuai);    
+#	return params->{bankuai};
+    return "the params is $bankuai no article found" unless @articles;
+    return template 'mitbbs/index.tt', { articles => \@articles };
     };
+
 };
 
 
--- lib/Mitbbs/Article.pm
+++ lib/Mitbbs/Article.pm
@@ -1,9 +1,28 @@
 package Mitbbs::Article;
-
+use v5.14;
+use Encode;
 use Moo;
+use Mojo::UserAgent;
+my $ua = Mojo::UserAgent->new();
+
+
 
 has 'link' => ( is => 'ro');
 has 'title'=> ( is => 'ro');
 
+sub content {
+    my $self = shift ;
+    my $full_href = 'http://www.mitbbs.com' . $self->link;
+    my $text = $ua->get($full_href)->res->dom->at('td[class^="jiawenzhang"]');
+    my $dom = Mojo::DOM->new($text);
+
+    return substr decode('gb2312',$dom->at('p')->text), 0, 500;
+}
+
+#header (author time),
+#content
+#footer( not necessary)  
+#my $art = Mitbbs::Article->new( link => '/article_t/RuralChina/31303515.html');
+#say $art->content;
 1;
 
