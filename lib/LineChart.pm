3ac5148680f4df460bba14ff3cdb709c029f2463
Hao Wu <hwu@intellisurvey.com>
2013-12-10 16:54:25 -0800

    add linechart

---
+++ lib/LineChart.pm
@@ -0,0 +1,20 @@
+package linechart;
+use Dancer2;
+set serializer => 'JSON';
+
+our $VERSION = '0.1';
+
+get '/count' => sub {
+    my ($min,$max) = (150,180); 
+    my $current_count = param "counts";
+    my $data = { 'seconds_elapsed' => $current_count,
+                 'beats_per_minute' => int(rand(0.3) * 100 + 150)
+                };
+    return  { success => \1,
+              data => $data,
+              results => 2    
+            };
+
+};
+
+true;
